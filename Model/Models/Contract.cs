4d30383e52f8da28801054d1dc3cb3f3bd02d25d
Nazmox <merse.sarvari@gmail.com>
2022-07-20 17:21:26 +0200

    WORKING version with Entity

--- Model/Models/Contract.cs
+++ Model/Models/Contract.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -23,5 +25,18 @@ namespace Models
 
         public virtual IList<Contract_Device> Contract_Device_Relations { get; set; }
 
+    }
+    public class ContractMap : ClassMapping<Contract>
+    {
+        public ContractMap()
+        {
+            Id(x => x.Id, map => map.Generator(Generators.Guid));
+            Property(x => x.Name, map => map.Length(150));
+            Property(x => x.Startdate, map => map.Length(150));
+            Property(x => x.Enddate, map => map.Length(150));
+            Property(x => x.Email, map => map.Length(150));
+            Property(x => x.Phone, map => map.Length(150));
+            Property(x => x.Address, map => map.Length(150));
+        }
     }
 }
